ade35d87506a02f0cbbc9c8aef31539543518f5f
ken-nah <kennyadduz@gmail.com>
2021-08-14 11:15:11 +0300

    Merge two sorted linked lists

---
+++ javascript/21.merge-two-sorted-lists.js
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=21 lang=javascript
+ *
+ * [21] Merge Two Sorted Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+var mergeTwoLists = function (l1, l2) {
+  let dummyHead = new ListNode(0);
+  let newList = dummyHead;
+
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      newList.next = l1;
+      l1 = l1.next;
+    } else {
+      newList.next = l2;
+      l2 = l2.next;
+    }
+    newList = newList.next;
+  }
+  if (l1) newList.next = l1;
+  if (l2) newList.next = l2;
+
+  return dummyHead.next;
+};
+// @lc code=end
